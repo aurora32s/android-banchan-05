1dbc1f235d70cb5f103493b233b0cfa335f5c714
aurora32s <aurora32s@naver.com>
2022-08-12 22:52:27 +0900

    [FEAT] #8 - 상세 화면 메뉴 정보 부분 UI 수정 - list item에서 collapsing toolbar 하위 component로 변경

--- app/src/main/java/com/seom/banchan/ui/adapter/viewholder/detail/DetailMenuViewHolder.kt
+++ app/src/main/java/com/seom/banchan/ui/adapter/viewholder/detail/DetailMenuViewHolder.kt
@@ -1,9 +1,9 @@
 package com.seom.banchan.ui.adapter.viewholder.detail
 
-<<<<<<< HEAD
 import com.seom.banchan.databinding.ItemMenuDetailBinding
 import com.seom.banchan.ui.adapter.viewholder.ModelViewHolder
 import com.seom.banchan.ui.model.detail.MenuDetailModel
+
 
 class DetailMenuViewHolder(
     private val binding: ItemMenuDetailBinding
@@ -12,17 +12,3 @@ class DetailMenuViewHolder(
         binding.menu = model
     }
 }
-=======
-import com.seom.banchan.databinding.ItemImageSliderBinding
-import com.seom.banchan.databinding.ItemMenuInfoBinding
-import com.seom.banchan.ui.adapter.viewholder.ModelViewHolder
-import com.seom.banchan.ui.model.detail.DetailMenuModel
-
-class DetailMenuViewHolder(
-    private val binding: ItemMenuInfoBinding
-) : ModelViewHolder<DetailMenuModel>(binding) {
-    override fun bind(model: DetailMenuModel) {
-        binding.detail = model
-    }
-}
->>>>>>> c419be4 ([FEAT] #8 - 상세 화면의 메뉴 정보와 상세 설명 이미지 UI 추가 완료)
