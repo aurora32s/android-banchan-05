4697c7272a740df84f62b7a8347b9a228ecf7cda
eshc123 <eshc.joon@gmail.com>
2022-08-12 09:08:15 +0900

    [FEAT] #18 - 기획전 이외 탭 관련 usecase 코드 추가

---
+++ app/src/main/java/com/seom/banchan/domain/usecase/GetMainMenusUseCase.kt
@@ -0,0 +1,25 @@
+package com.seom.banchan.domain.usecase
+
+import com.seom.banchan.di.IODispatcher
+import com.seom.banchan.domain.model.CategoryModel
+import com.seom.banchan.domain.model.MenuModel
+import com.seom.banchan.domain.repository.MenuRepository
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+/**
+ * 카테고리가 있는 메뉴 리스트 요청
+ */
+@ViewModelScoped
+class GetMainMenusUseCase @Inject constructor(
+    private val menuRepository: MenuRepository,
+    @IODispatcher
+    private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend operator fun invoke(): Result<List<MenuModel>> = withContext(ioDispatcher) {
+        menuRepository.getMainMenus()
+    }
+}
