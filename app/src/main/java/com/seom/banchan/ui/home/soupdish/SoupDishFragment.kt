9f3899b341863a7c390a1c2f5cda645d3f71c644
eshc123 <eshc.joon@gmail.com>
2022-08-18 20:41:10 +0900

    [FEAT] #31 ItemDecoration 수정

--- app/src/main/java/com/seom/banchan/ui/home/soupdish/SoupDishFragment.kt
+++ app/src/main/java/com/seom/banchan/ui/home/soupdish/SoupDishFragment.kt
@@ -65,7 +65,7 @@ class SoupDishFragment : Fragment() {
     private fun initRecyclerView() = binding?.let {
         it.rvSoupDish.adapter = homeAdapter
         it.rvSoupDish.setGridLayoutManager(requireContext())
-        it.rvSoupDish.addItemDecoration(GridItemDecoration(requireContext(),true))
+        it.rvSoupDish.addItemDecoration(GridItemDecoration(requireContext(),true,3))
         homeAdapter.submitList(
             getDefaultHeaders()
         )
