e2ddf5061efe45a0dd12a3c059ca9da7d301ae1f
aurora32s <aurora32s@naver.com>
2022-08-18 10:41:25 +0900

    [FIX] #45 - Database 충돌에 의해 손실된 코드 추가

--- app/src/main/java/com/seom/banchan/di/DatabaseModule.kt
+++ app/src/main/java/com/seom/banchan/di/DatabaseModule.kt
@@ -2,7 +2,6 @@ package com.seom.banchan.di
 
 import android.content.Context
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import com.seom.banchan.data.db.BanChanDatabase
 import com.seom.banchan.data.db.dao.RecentlyDao
 import dagger.Module
