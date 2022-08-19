7705c21d2e2ba20e1479951815fd6d50db8f2e43
aurora32s <aurora32s@naver.com>
2022-08-20 01:51:05 +0900

    [FEAT] #59 - 주문 내역 리스트 화면 viewHolder 생성 및 Mapper binding

---
+++ app/src/main/java/com/seom/banchan/ui/adapter/viewholder/order/OrderListItemViewHolder.kt
@@ -0,0 +1,20 @@
+package com.seom.banchan.ui.adapter.viewholder.order
+
+import com.seom.banchan.databinding.ItemOrderBinding
+import com.seom.banchan.ui.adapter.viewholder.ModelViewHolder
+import com.seom.banchan.ui.model.order.OrderListItemModel
+import com.seom.banchan.util.listener.ModelAdapterListener
+
+class OrderListItemViewHolder(
+    private val binding: ItemOrderBinding
+) : ModelViewHolder<OrderListItemModel>(binding) {
+
+    override fun bindData(model: OrderListItemModel) {
+        super.bindData(model)
+    }
+
+    override fun bindViews(model: OrderListItemModel, menuAdapterListener: ModelAdapterListener?) {
+        super.bindViews(model, menuAdapterListener)
+    }
+
+}
