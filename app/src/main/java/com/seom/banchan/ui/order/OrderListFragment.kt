136fa34c273dc4f6b08a55ffaf16c4f5f81a7d16
aurora32s <aurora32s@naver.com>
2022-08-20 01:35:49 +0900

    [FEAT] #59 - 주문 내역 리스트 XML layout 코드 추가

--- app/src/main/java/com/seom/banchan/ui/order/OrderListFragment.kt
+++ app/src/main/java/com/seom/banchan/ui/order/OrderListFragment.kt
@@ -1,8 +1,25 @@
 package com.seom.banchan.ui.order
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.seom.banchan.databinding.FragmentOrderListBinding
 import com.seom.banchan.ui.base.BaseFragment
 
 class OrderListFragment : BaseFragment() {
+
+    private var _binding: FragmentOrderListBinding? = null
+    private val binding = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOrderListBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
 
     companion object {
         const val TAG = ".OrderListFragment"
