332f2efe9875c7703d5c4be0e9ead696bcf22761
Jung SeokJoon <eshc.joon@gmail.com>
2022-08-21 14:14:04 +0900

    [REFACTOR] #65 불필요한 주석 삭제

--- app/src/main/java/com/seom/banchan/data/db/entity/RecentEntity.kt
+++ app/src/main/java/com/seom/banchan/data/db/entity/RecentEntity.kt
@@ -11,7 +11,7 @@ data class RecentEntity(
     @PrimaryKey @ColumnInfo(name = "recent_id") val id: String,
     @ColumnInfo(name = "image") val image: String,
     @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "description") val description: String, // 최근 본 상품 화면에서 상세보기로 이동 시 필요해서 추가
+    @ColumnInfo(name = "description") val description: String,
     @ColumnInfo(name = "normal_price") val normalPrice: Int,
     @ColumnInfo(name = "sale_price") val salePrice: Int,
     @ColumnInfo(name = "recent_time") val recentTime: Long = System.currentTimeMillis()
