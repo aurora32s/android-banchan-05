2035de90262406bbf79a27dd3ac8385088c3c4d0
aurora32s <aurora32s@naver.com>
2022-08-23 02:39:46 +0900

    [FEAT] #61 - BOOT Complete Broadcast 추가 이후에 배달이 완료되지 않은 주문에 대해 알림을 재 생성하는 코드 추가하기

---
+++ app/src/main/java/com/seom/banchan/worker/system/BootCompleteReceiver.kt
@@ -0,0 +1,15 @@
+package com.seom.banchan.worker.system
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootCompleteReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.let {
+            if (intent.action == "android.intent.action.BOOT_COMPLETE") {
+                // TODO 배달이 완료되지 않은 주문에 대해 알림 재생성
+            }
+        } ?: return
+    }
+}
