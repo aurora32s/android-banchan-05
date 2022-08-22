bc0c5a53688510b626799fb38fc94d3a8bd79d75
aurora32s <aurora32s@naver.com>
2022-08-23 03:14:07 +0900

    [FEAT] #61 - 배달이 완료되었을 때 Notification 띄워주기

--- app/src/main/java/com/seom/banchan/worker/alarm/DeliveryAlarmReceiver.kt
+++ app/src/main/java/com/seom/banchan/worker/alarm/DeliveryAlarmReceiver.kt
@@ -1,19 +1,27 @@
 package com.seom.banchan.worker.alarm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.util.Log
+import com.seom.banchan.BuildConfig
+import com.seom.banchan.worker.model.DeliveryAlarmModel
+import com.seom.banchan.worker.notification.DeliveryNotificationManager
 
 class DeliveryAlarmReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent?) {
         val deliveryInfo = intent?.getSerializableExtra(KEY_ORDER_ITEM) ?: return
-        Log.d(TAG, "on Received $deliveryInfo")
+        (deliveryInfo as? DeliveryAlarmModel)?.let {
+            DeliveryNotificationManager.create(context, deliveryInfo)
+        }
     }
 
     companion object {
         const val TAG = ".DeliveryAlarmReceiver"
-
         const val KEY_ORDER_ITEM = "order_item"
     }
 }
---
+++ app/src/main/java/com/seom/banchan/worker/notification/DeliveryNotificationManager.kt
@@ -0,0 +1,82 @@
+package com.seom.banchan.worker.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.seom.banchan.R
+import com.seom.banchan.ui.main.MainActivity
+import com.seom.banchan.worker.model.DeliveryAlarmModel
+
+object DeliveryNotificationManager {
+    fun create(context: Context, deliveryAlarmModel: DeliveryAlarmModel) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel(notificationManager)
+        buildNotification(
+            context,
+            notificationManager,
+            deliveryAlarmModel
+        )
+    }
+
+    fun createNotificationChannel(notificationManager: NotificationManager) {
+        // Oreo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.description = CHANNEL_DESC
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun buildNotification(
+        context: Context,
+        notificationManager: NotificationManager,
+        deliveryAlarmModel: DeliveryAlarmModel
+    ) {
+        val contentIntent = Intent(context, MainActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            context,
+            deliveryAlarmModel.orderId.toInt(),
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_app_icon)
+            .setContentTitle("배달이 도착했어요!")
+            .setContentText(
+                "" +
+                        "${deliveryAlarmModel.menuName} " +
+                        if (deliveryAlarmModel.totalCount > 1)
+                            "외 ${deliveryAlarmModel.totalCount - 1}개"
+                        else ""
+            )
+            .setContentIntent(contentPendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .run {
+                notificationManager.notify(
+                    deliveryAlarmModel.orderId.toInt(),
+                    this.build()
+                )
+            }
+    }
+
+    private const val CHANNEL_ID = "notification_channel"
+    private const val CHANNEL_NAME = "배달 알림 채널"
+    private const val CHANNEL_DESC = "주문하신 배달이 완료되었을 떄 알려주는 채널입니다."
+}
