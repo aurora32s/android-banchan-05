78458a959eb7686f12d9e833ea92406524b6fe45
aurora32s <aurora32s@naver.com>
2022-08-23 20:24:36 +0900

    [FEAT] #71 - 최근 본 상품 화면에서도 장바구니에 있는 메뉴 UI 적용하기

--- app/src/main/java/com/seom/banchan/ui/recent/RecentViewModel.kt
+++ app/src/main/java/com/seom/banchan/ui/recent/RecentViewModel.kt
@@ -2,7 +2,9 @@ package com.seom.banchan.ui.recent
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.seom.banchan.domain.model.home.MenuModel
 import com.seom.banchan.domain.model.home.toHomeMenuModel
+import com.seom.banchan.domain.usecase.GetCartMenusIdUseCase
 import com.seom.banchan.domain.usecase.GetRecentMenusUseCase
 import com.seom.banchan.ui.model.CellType
 import com.seom.banchan.ui.model.home.HomeMenuModel
@@ -10,24 +12,32 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RecentViewModel @Inject constructor(
-    private val getRecentMenusUseCase: GetRecentMenusUseCase
+    private val getRecentMenusUseCase: GetRecentMenusUseCase,
+    getCartMenusIdUseCase: GetCartMenusIdUseCase
 ) : ViewModel() {
 
-    private val _recentMenus = MutableStateFlow<List<HomeMenuModel>>(emptyList())
-    val recentMenus: StateFlow<List<HomeMenuModel>>
-        get() = _recentMenus
+    private val cartMenus = getCartMenusIdUseCase()
+    private val _recentMenus = MutableStateFlow<List<MenuModel>>(emptyList())
+    val recentMenus = _recentMenus
+        .combine(cartMenus) { menus, carts ->
+            menus.map {
+                it.toHomeMenuModel(
+                    cartMenus = carts,
+                    cellType = CellType.MENU_RECENT_CELL
+                )
+            }
+        }
 
     fun getRecentMenus() {
         viewModelScope.launch {
             getRecentMenusUseCase().collectLatest {
-                _recentMenus.value = it.map {
-                    it.toHomeMenuModel(cellType = CellType.MENU_RECENT_CELL)
-                }
+                _recentMenus.value = it
             }
         }
     }
