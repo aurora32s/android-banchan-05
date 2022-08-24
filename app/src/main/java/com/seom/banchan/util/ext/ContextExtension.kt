bab1b124c24382593b3af908d4d046eefe0f9bff
aurora32s <aurora32s@naver.com>
2022-08-25 01:01:13 +0900

    [REFACTOR] #101 - 주문 내역 리스트 화면에서 데이터가 없는 경우 또는 에러가 발생하는 경우 UI 처리

--- app/src/main/java/com/seom/banchan/util/ext/ContextExtension.kt
+++ app/src/main/java/com/seom/banchan/util/ext/ContextExtension.kt
@@ -1,11 +1,27 @@
 package com.seom.banchan.util.ext
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
+import androidx.annotation.StringRes
+import com.google.android.material.snackbar.Snackbar
+import com.seom.banchan.R
 
 /**
  * toast
  */
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+/**
+ * snack bar
+ */
+fun Context.snackBarForReRequest(
+    root: View,
+    onClick: View.OnClickListener
+) {
+    Snackbar.make(root, R.string.msg_data_re_request, Snackbar.LENGTH_LONG)
+        .setAction(R.string.btn_re_request, onClick)
+        .show()
 }
