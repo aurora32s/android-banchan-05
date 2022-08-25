47540fafba19bf240229fff9bbdab8c9fb078235
aurora32s <aurora32s@naver.com>
2022-08-25 21:40:25 +0900

    [REFACTOR] #58 - fragment / viewModel 코드 정리

--- app/src/main/java/com/seom/banchan/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/seom/banchan/ui/detail/DetailViewModel.kt
@@ -3,10 +3,8 @@ package com.seom.banchan.ui.detail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.seom.banchan.domain.model.cart.CartMenuModel
-import com.seom.banchan.domain.model.detail.DetailMenuModel
 import com.seom.banchan.domain.model.detail.toUiModel
 import com.seom.banchan.domain.model.home.MenuModel
-import com.seom.banchan.domain.model.order.OrderListModel
 import com.seom.banchan.domain.usecase.*
 import com.seom.banchan.ui.model.detail.DetailMenuUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,21 +17,16 @@ class DetailViewModel @Inject constructor(
     private val getMenuDetailUseCase: GetMenuDetailUseCase,
     private val addOrUpdateMenuToCartUseCase: AddOrUpdateMenuToCartUseCase,
     private val upsertRecentMenuUseCase: UpsertRecentMenuUseCase,
-    private val getCartMenusUseCase: GetCartMenusUseCase,
-    private val getOrderListUseCase: GetOrderListUseCase
+    getCartMenusUseCase: GetCartMenusUseCase,
+    getOrderListUseCase: GetOrderListUseCase
 ) : ViewModel() {
-    private val _detailMenuUiModel = MutableStateFlow<DetailUiState>(DetailUiState.UnInitialized)
-    val detailMenuModel: StateFlow<DetailUiState>
-        get() = _detailMenuUiModel
 
-    private val _cartMenus = getCartMenusUseCase()
-    val cartMenus: Flow<List<CartMenuModel>>
-        get() = _cartMenus
+    private val _detailUiState = MutableStateFlow<DetailUiState>(DetailUiState.UnInitialized)
+    val detailUiState = _detailUiState.asStateFlow()
 
-
-    private val _orderList = getOrderListUseCase()
-    val orderList: Flow<List<OrderListModel>>
-        get() = _orderList
+    // 상단 appbar 에 필요한 정보
+    val cartMenus = getCartMenusUseCase() // 장바구니 메뉴 정보
+    val orderList = getOrderListUseCase() // 주문 정보
 
     // 선택한 음식의 개수
     private val _count = MutableStateFlow(1)
@@ -54,54 +47,58 @@ class DetailViewModel @Inject constructor(
         _totalPrice.asStateFlow().combine(count) { newCount, salePrice -> newCount * salePrice }
 
     // 현재 음식 data
-    lateinit var currentMenu: DetailMenuModel
+    private val _currentMenu = MutableStateFlow<DetailMenuUiModel?>(null)
+    val currentMenu = _currentMenu.asStateFlow()
 
-    fun init() {
-        _detailMenuUiModel.value = DetailUiState.UnInitialized
-    }
-
-    fun fetchData(menu: MenuModel?) = viewModelScope.launch {
+    fun fetchData(menu: MenuModel?) {
         if (menu == null) {
-            _detailMenuUiModel.value = DetailUiState.Error
-            return@launch
+            _detailUiState.value = DetailUiState.FailFetchDetail
+            return
         }
-        getMenuDetailUseCase(menu.id)
-            .onSuccess {
-                currentMenu = it
-                _totalPrice.value = it.salePrice
-                _detailMenuUiModel.value =
-                    DetailUiState.Success.SuccessFetch(currentMenu.toUiModel())
-                // 최근 본 상품으로 등록
-                upsertRecentMenuUseCase(menu)
-            }
+        viewModelScope.launch {
+            getMenuDetailUseCase(menu.id)
+                .onSuccess {
+                    _currentMenu.value = it.toUiModel(menu.name)
+                    _totalPrice.value = it.salePrice
+                    // 최근 본 상품으로 등록
+                    upsertRecentMenuUseCase(menu)
+                }
+                .onFailure {
+                    _detailUiState.value = DetailUiState.FailFetchDetail
+                }
+        }
     }
 
-    fun addMenuToCart() = viewModelScope.launch {
-        val cartMenu = CartMenuModel(
-            menuId = currentMenu.id,
-            name = currentMenu.name,
-            image = currentMenu.thumbnail?.firstOrNull(),
-            salePrice = currentMenu.salePrice,
-            count = count.value,
-            selected = true // 기본은 선택된 상태
-        )
-        addOrUpdateMenuToCartUseCase(cartMenu)
-            .onSuccess {
-                _detailMenuUiModel.value = DetailUiState.Success.SuccessAddToCart
+    fun addMenuToCart() {
+        viewModelScope.launch {
+            _detailUiState.value = DetailUiState.Loading
+            _currentMenu.value?.let {
+                val detailMenu = it.detailMenu
+                val cartMenu = CartMenuModel(
+                    menuId = detailMenu.id,
+                    name = detailMenu.name,
+                    image = detailMenu.images?.firstOrNull(),
+                    salePrice = detailMenu.salePrice,
+                    count = count.value,
+                    selected = true // 기본은 선택된 상태
+                )
+                addOrUpdateMenuToCartUseCase(cartMenu)
+                    .onSuccess {
+                        _detailUiState.value = DetailUiState.SuccessAddToCart
+                    }
+                    .onFailure {
+                        _detailUiState.value = DetailUiState.FailAddToCart
+                    }
             }
+        }
     }
 
 }
 
 sealed interface DetailUiState {
-    object UnInitialized : DetailUiState
-    sealed interface Success : DetailUiState {
-        data class SuccessFetch(
-            val detailMenu: DetailMenuUiModel
-        ) : Success
-
-        object SuccessAddToCart : Success
-    }
-
-    object Error : DetailUiState
+    object UnInitialized : DetailUiState // 초기
+    object Loading : DetailUiState
+    object SuccessAddToCart : DetailUiState // 장바구니 추가 성송
+    object FailFetchDetail : DetailUiState // 상세 정보 요청 실패
+    object FailAddToCart : DetailUiState // 장바구니 추가 실패
 }
