2a6f6ca1ec00e0159382105177a7c80206bbc9d1
Jung SeokJoon <eshc.joon@gmail.com>
2022-08-25 21:38:59 +0900

    [FEATURE] #106 CartDao,RecentDao 테스트코드 추가, OrderDao 수정

--- app/src/androidTest/java/com/seom/banchan/CartDaoTest.kt
+++ app/src/androidTest/java/com/seom/banchan/CartDaoTest.kt
@@ -19,8 +19,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class CartDaoTest {
 
-    private lateinit var database : BanChanDatabase
-    private lateinit var cartDao : CartDao
+    private lateinit var database: BanChanDatabase
+    private lateinit var cartDao: CartDao
     private val cartMenuEntity = CartMenuEntity(
         menuId = "HBDEF",
         name = "오리 주물럭_반조리",
@@ -50,7 +50,7 @@ class CartDaoTest {
     )
 
     @Before
-    fun setup(){
+    fun setup() {
         database = Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),
             BanChanDatabase::class.java
@@ -61,7 +61,7 @@ class CartDaoTest {
     }
 
     @After
-    fun cleanUp(){
+    fun cleanUp() {
         database.close()
     }
 
@@ -77,9 +77,11 @@ class CartDaoTest {
 
     @Test
     fun `이미_추가된_상품을_다시_추가하면_해당_상품은_업데이트된다`() = runTest {
-        cartDao.insertOrReplaceCartMenu(cartMenuEntity.copy(
-            name = "오리 주물럭_조리"
-        ))
+        cartDao.insertOrReplaceCartMenu(
+            cartMenuEntity.copy(
+                name = "오리 주물럭_조리"
+            )
+        )
         val menu = cartDao.getCartMenuById(cartMenuEntity.menuId)
 
         assertThat(
@@ -91,9 +93,11 @@ class CartDaoTest {
 
     @Test
     fun `메뉴ID를_통해_카트에_담긴_상품을_조회할_수_있다`() = runTest {
-        cartDao.insertOrReplaceCartMenu(cartMenuEntity.copy(
-            name = "오리 주물럭_반조리"
-        ))
+        cartDao.insertOrReplaceCartMenu(
+            cartMenuEntity.copy(
+                name = "오리 주물럭_반조리"
+            )
+        )
         val cartMenu = cartDao.getCartMenuById("HBDEF")
         assertThat(
             this@CartDaoTest.cartMenuEntity.name == cartMenu?.name
@@ -125,14 +129,33 @@ class CartDaoTest {
 
     @Test
     fun `선택_상태인_상품읆_모두_삭제할_수_있다`() = runTest { // 선택한 메뉴가 삭제됐는가
+        val cartMenus = cartMenuEntityList.map {
+            it.copy(selected = true)
+        }
+        cartDao.deleteCartMenuList(
+            cartMenus.filter {
+                it.selected == true
+            }.map {
+                it.menuId
+            }
+        )
 
+        assertThat(
+            cartDao.getCartMenus().first().filter {
+                cartMenus.map {
+                    it.menuId
+                }.contains(it.menuId)
+            }.size == 0
+        ).isTrue()
     }
 
     @Test
     fun `메뉴ID를_통해_상품을_선택_상태로_변경할_수_있다`() = runTest { // 해당 메뉴가 선택 상태로 됐는가
-        cartDao.insertOrReplaceCartMenu(cartMenuEntity.copy(
-            selected = false
-        ))
+        cartDao.insertOrReplaceCartMenu(
+            cartMenuEntity.copy(
+                selected = false
+            )
+        )
         cartDao.updateCartMenuSelected(cartMenuEntity.menuId)
 
         val cartMenu = cartDao.getCartMenuById(cartMenuEntity.menuId)
@@ -143,19 +166,56 @@ class CartDaoTest {
 
     @Test
     fun `상품들을_모두_전체_선택_상태로_변경할_수_있다`() = runTest { // 전체 선택이 제대로 됐는가
+        val cartMenus = cartMenuEntityList.map {
+            it.copy(selected = false)
+        }
+        cartMenus.forEach {
+            cartDao.insertOrReplaceCartMenu(it)
+        }
+        cartDao.updateCartMenuListSelected(
+            cartMenus.filter {
+                it.selected == false
+            }.map {
+                it.menuId
+            }
+        )
+        assertThat(
+            cartDao.getCartMenus().first().filter {
+                it.selected == false
+            }.size == 0
+        ).isTrue()
 
     }
 
     @Test
     fun `선택된_상품들을_모두_선택_해제_상태로_변경할_수_있다`() = runTest {
-
+        val cartMenus = cartMenuEntityList.map {
+            it.copy(selected = true)
+        }
+        cartMenus.forEach {
+            cartDao.insertOrReplaceCartMenu(it)
+        }
+        cartDao.updateCartMenuListSelected(
+            cartMenus.filter {
+                it.selected == true
+            }.map {
+                it.menuId
+            }
+        )
+        assertThat(
+            cartDao.getCartMenus().first().filter {
+                it.selected == true
+            }.size == 0
+        ).isTrue()
     }
 
     @Test
     fun `상품의_개수를_변경할_수_있다`() = runTest { // 메뉴 개수 변경이 제대로 됐는가
-        cartDao.insertOrReplaceCartMenu(cartMenuEntity.copy(
-            count = 1
-        ))
+        cartDao.insertOrReplaceCartMenu(
+            cartMenuEntity.copy(
+                count = 1
+            )
+        )
         cartDao.updateCartMenuCount(cartMenuEntity.menuId, 20)
 
         val cartMenu = cartDao.getCartMenuById(cartMenuEntity.menuId)
@@ -166,9 +226,11 @@ class CartDaoTest {
 
     @Test
     fun `싱픔의_개수를_1_증가시킬_수_있다`() = runTest { // 메뉴 개수 증가가 제대로 됐는가
-        cartDao.insertOrReplaceCartMenu(cartMenuEntity.copy(
-            count = 1
-        ))
+        cartDao.insertOrReplaceCartMenu(
+            cartMenuEntity.copy(
+                count = 1
+            )
+        )
         cartDao.updateCartMenuCountIncrease(cartMenuEntity.menuId)
 
         val cartMenu = cartDao.getCartMenuById(cartMenuEntity.menuId)
@@ -179,9 +241,11 @@ class CartDaoTest {
 
     @Test
     fun `상품의_개수를_1_감소시킬_수_있다`() = runTest { // 메뉴 개수 감소가 제대로 됐는가
-        cartDao.insertOrReplaceCartMenu(cartMenuEntity.copy(
-            count = 30
-        ))
+        cartDao.insertOrReplaceCartMenu(
+            cartMenuEntity.copy(
+                count = 30
+            )
+        )
         cartDao.updateCartMenuCountDecrease(cartMenuEntity.menuId)
 
         val cartMenu = cartDao.getCartMenuById(cartMenuEntity.menuId)
--- app/src/androidTest/java/com/seom/banchan/RecentDaoTest.kt
+++ app/src/androidTest/java/com/seom/banchan/RecentDaoTest.kt
@@ -3,10 +3,13 @@ package com.seom.banchan
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
 import com.seom.banchan.data.db.BanChanDatabase
 import com.seom.banchan.data.db.dao.RecentDao
 import com.seom.banchan.data.db.entity.RecentEntity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -16,8 +19,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class RecentDaoTest {
 
-    private lateinit var database : BanChanDatabase
-    private lateinit var recentDao : RecentDao
+    private lateinit var database: BanChanDatabase
+    private lateinit var recentDao: RecentDao
     private val recentEntity = RecentEntity(
         id = "HBDEF",
         title = "오리 주물럭_반조리",
@@ -45,12 +48,12 @@ class RecentDaoTest {
             salePrice = 11610,
             normalPrice = 12900,
             description = "탱글한 면발과 맛깔진 고명이 가득",
-            recentTime = System.currentTimeMillis()-14000L
+            recentTime = System.currentTimeMillis() - 14000L
         )
     )
 
     @Before
-    fun setup(){
+    fun setup() {
         database = Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),
             BanChanDatabase::class.java
@@ -61,22 +64,154 @@ class RecentDaoTest {
     }
 
     @After
-    fun cleanUp(){
+    fun cleanUp() {
         database.close()
     }
 
     @Test
-    fun `모든_최근_본_상품들을_조회할_수_있다`(){
+    fun `최근_본_상품을_추가할_수_있다`() = runTest {
 
+        recentDao.upsertRecent(recentEntity)
+
+        val recentMenus = recentDao.getRecents()
+        assertThat(
+            recentMenus.first().contains(recentEntity)
+        ).isTrue()
     }
 
     @Test
-    fun `최대_7개의_최근_본_상품들을_조회할_수_있다`(){
+    fun `최근_본_상품이_만약에_이미_존재한다면_변경할_수_있다`() = runTest {
+        val recentMenu = recentEntity
+        recentDao.upsertRecent(
+            recentMenu.copy(
+                title = "오리 주물럭_반조리"
+            )
+        )
 
+        val recentMenus = recentDao.getRecents()
+        assertThat(
+            recentMenus.first().filter {
+                it.id == recentMenu.id
+            }.first().title == "오리 주물럭 반조리"
+        ).isTrue()
     }
 
     @Test
-    fun `최근_본_상품을_추가하거나_만약에_이미_존재한다면_변경할_수_있다`(){
+    fun `모든_최근_본_상품들을_조회할_수_있다`() = runTest {
+        recentMenuEntityList.forEach {
+            recentDao.upsertRecent(it)
+        }
 
+        val recentMenus = recentDao.getRecents()
+
+        assertThat(
+            recentMenus.first() == recentMenuEntityList
+        ).isTrue()
+    }
+
+    @Test
+    fun `최대_7개의_최근_본_상품들을_조회할_수_있다`() = runTest {
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "1", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis()
+            )
+        )
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "2", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 100
+            )
+        )
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "3", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 110
+            )
+        )
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "4", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 120
+            )
+        )
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "5", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 130
+            )
+        )
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "6", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 140
+            )
+        )
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "7", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 150
+            )
+        )
+        recentDao.upsertRecent(
+            RecentEntity(
+                id = "8", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 160
+            )
+        )
+
+        val latestRecentMenus = recentDao.getLatestRecents()
+        assertThat(
+            latestRecentMenus.first().size == 7
+        ).isTrue()
+    }
+
+    @Test
+    fun `최대_7개의_최근_본_상품들을_최근_순서대로_조회할_수_있다`() = runTest {
+        val recentList = listOf(
+            RecentEntity(
+                id = "1", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis()
+            ),
+            RecentEntity(
+                id = "5", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 500
+            ),
+            RecentEntity(
+                id = "3", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 300
+            ),
+            RecentEntity(
+                id = "6", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 600
+            ),
+            RecentEntity(
+                id = "4", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 400
+            ),
+            RecentEntity(
+                id = "2", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 200
+            ),
+            RecentEntity(
+                id = "7", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 700
+            ),
+            RecentEntity(
+                id = "8", image = "", title = "", description = "", normalPrice = 0, salePrice = 0,
+                recentTime = System.currentTimeMillis() - 800
+            )
+        )
+
+        recentList.forEach {
+            recentDao.upsertRecent(it)
+        }
+
+        val latestRecentMenus = recentDao.getLatestRecents()
+        assertThat(
+            latestRecentMenus.first() == recentList.sortedByDescending {
+                it.recentTime
+            }.subList(0,7)
+        ).isTrue()
     }
 }
