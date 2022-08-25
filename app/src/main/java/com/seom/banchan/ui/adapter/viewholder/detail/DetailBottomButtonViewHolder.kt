6e1b364068615d4397131eda2cfe2fdde1331baf
aurora32s <aurora32s@naver.com>
2022-08-25 21:46:12 +0900

    [REFACTOR] #58 - viewModel의 CoroutineScope 제거

--- app/src/main/java/com/seom/banchan/ui/adapter/viewholder/detail/DetailBottomButtonViewHolder.kt
+++ app/src/main/java/com/seom/banchan/ui/adapter/viewholder/detail/DetailBottomButtonViewHolder.kt
@@ -1,8 +1,11 @@
 package com.seom.banchan.ui.adapter.viewholder.detail
 
+import androidx.core.view.doOnAttach
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.seom.banchan.databinding.ItemDetailBottomButtonBinding
 import com.seom.banchan.ui.adapter.viewholder.ModelViewHolder
 import com.seom.banchan.ui.model.detail.DetailBottomButtonModel
+import com.seom.banchan.util.ext.repeatLaunch
 import com.seom.banchan.util.listener.ModelAdapterListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,8 +14,21 @@ import kotlinx.coroutines.launch
 class DetailBottomButtonViewHolder(
     private val binding: ItemDetailBottomButtonBinding
 ) : ModelViewHolder<DetailBottomButtonModel>(binding) {
+
+    private lateinit var model: DetailBottomButtonModel
+
+    init {
+        itemView.doOnAttach {
+            if (::model.isInitialized) initObserver(model)
+        }
+    }
+
     override fun bindData(model: DetailBottomButtonModel) {
-        CoroutineScope(Dispatchers.IO).launch {
+        this.model = model
+    }
+
+    private fun initObserver(model: DetailBottomButtonModel) {
+        binding.root.findViewTreeLifecycleOwner()?.repeatLaunch {
             model.totalCount.collect {
                 binding.totalPrice = it
             }
